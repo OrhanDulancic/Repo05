c66fdc7f169238bfa7d9ea47f9681daa7388f102
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-01 18:30:30 +0200

    MadLib

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this code
+        var myNoun = "dog";
+        var myVerb = "ran";
+        var myAdjective = "big";
+        var myAdverb = "quickly";
+        var wordBlanks = "";
+        wordBlanks = "My " + myNoun + " is " + myAdjective + " and he " + myVerb + ", " + myAdverb + ".";
+    // Only chagne code above this code
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
