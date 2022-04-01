0a0150149d3245cad4e8d9daff40631ed1889d61
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-01 13:04:52 +0200

    String Variables

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myHomeTown = "Mostar";
+    var myFirstName = "Orhan";
+    var myLastName = "Dulancic";
+    // only change ccode above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
