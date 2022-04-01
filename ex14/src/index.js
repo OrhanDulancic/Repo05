2ed6eb863621f9df7b16f721e4bd9214a4e39091
OrhanDulancic <orhan.dulancic@outlook.com>
2022-04-01 17:56:35 +0200

    third to last

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+        var lastName = "Bezos";
+        var thirdToLetterOfLastName = lastName[lastName.length - 3];
+    // Only change code above this line
+    return thirdToLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
